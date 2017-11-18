79c437a89bea5444816403295298d09a4827729b
Palle Klewitz <palle.klewitz@tum.de>
2017-11-18 18:28:18 +0100

    commit

--- main.py
+++ main.py
@@ -63,23 +63,23 @@ def create_classification_model(include_top=True,
 
     img_input = Input(shape=input_shape)
     # Block 1
-    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
     # Block 2
-    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
     # Block 3
-    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
     # Block 4
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
     # Block 5
-    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
     if include_top:
@@ -150,7 +150,7 @@ def load_dataset(dataset_root):
         target_size=(68, 120),
         color_mode='rgb',
         class_mode='categorical',
-        batch_size=32,
+        shuffle=True
     )
 
 
